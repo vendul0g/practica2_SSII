ff490f80affc1194c80cff61ebc4d47b9752d027
Alvaro Navarro <a.navarromartinez1@um.es>
2023-02-12 13:13:31 +0100

    -h

--- git_commit.sh
+++ git_commit.sh
@@ -3,28 +3,24 @@ if test $# -eq 0
 then
   echo "commit.sh: Meteme el mensaje para el comit entre comillas dobles \"\" "
   exit
+
 else
   if test $# -gt 1
   then
     echo "demasiados argumentos"
     exit
+  
+  else
+    if test $1="-h" 
+    then
+      echo "commit.sh: Meteme el mensaje para el commit entre comillas dobles \"\" "
+    fi
   fi
 fi
 
 #echo $1
 git add -A
 git commit -m "$1"
-
-expect - <<_END_EXPECT
-  spawn git push -u origin main
-  expect 'User*'
-  send "vendul0g\r"
-  expect 'Pass*'
-  send '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\r'
-  set timeout -1 ; #no timeout
-  expect eof
-_END_EXPECT
-
+git push -u origin main
 #usuario github:	vendul0g
 #token github: 		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
-
