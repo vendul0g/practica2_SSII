34b529c77baf43a57bfd29a4027ede629d1e0695
Alvaro Navarro <a.navarromartinez1@um.es>
2023-02-12 12:51:37 +0100

    prueba

--- git_commit.sh
+++ git_commit.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 if test $# -eq 0
 then
-	echo "commit.sh: Meteme el mensaje para el comit entre comillas dobles \"\" "
-	exit
+  echo "commit.sh: Meteme el mensaje para el comit entre comillas dobles \"\" "
+  exit
 else
-	if test $# -gt 1
-		then
-			echo "demasiados argumentos"
-			exit
-	fi
+  if test $# -gt 1
+  then
+    echo "demasiados argumentos"
+    exit
+  fi
 fi
 
 #echo $1
 git add -A
 git commit -m "$1"
-spawn git push -u origin main
-expect 'User*'
-send "vendul0g\r"
-expect 'Pass*'
-send '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'
+
+expect - << EOF
+  spawn git push -u origin main
+  expect 'User*'
+  send "vendul0g\r"
+  expect 'Pass*'
+  send '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'
+EOF
+
 #usuario github:	vendul0g
 #token github: 		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
 
